08ceea99245b0e52b5ef59f4ee750d59a9bc86d5
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2022-05-09 21:40:25 +0200

    Adds core and evil packages

---
+++ packages/evil.el
@@ -0,0 +1,18 @@
+;; install packages
+(dolist (package '(evil evil-collection evil-commentary evil-surround magit))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;; evil configuration
+(setq evil-undo-system 'undo-redo)
+(setq evil-want-C-u-scroll t)
+(setq evil-want-keybinding nil)
+
+(require 'evil)
+(when (require 'evil-collection nil t)
+  (evil-collection-init))
+
+(evil-commentary-mode)
+(setq global-evil-surround-mode 1)
+(evil-mode 1)
