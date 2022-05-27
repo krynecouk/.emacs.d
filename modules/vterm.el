cf1dbde0ac339aaf5083d9b2d61dd83584a20e8f
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2022-05-27 13:41:51 +0200

    Adds better vterm configuration

--- modules/vterm.el
+++ modules/vterm.el
@@ -1,1 +1,9 @@
-(use-package vterm)
+(use-package vterm
+  :config
+  (defun hide-line-numbers ()
+    (display-line-numbers-mode 0))
+  :hook (vterm-mode . hide-line-numbers))
+
+(use-package vterm-toggle
+  :custom
+  (vterm-toggle-scope 'project))
