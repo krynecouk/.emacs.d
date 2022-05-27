7bdde053e7a47fd2bb305c3c2a007faaf2cb8eb6
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2022-05-27 13:44:32 +0200

    Adds project config

---
+++ modules/project.el
@@ -0,0 +1,7 @@
+(use-package project
+  :custom
+  (project-switch-commands
+   '((project-find-file "Find file")
+     (magit-project-status "Magit" ?g)
+     (deadgrep "Grep" ?*)
+     (vterm-toggle "Term" ?t))))
