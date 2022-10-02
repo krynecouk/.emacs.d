cc2c2e6f6d4b64067971db18dc7c750d2fea6af5
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2022-10-02 12:54:16 +0200

    Adds script that loads the host's .zshrc

--- modules/core.el
+++ modules/core.el
@@ -54,3 +54,11 @@
 
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
+
+;; copy env variables from ~/.zshrc
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
