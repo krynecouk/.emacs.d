8fa8901c826b6414a6d64ffc7f6bc342ee9948f1
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2026-01-07 10:39:25 +0100

    Better dirvish bindings

--- modules/dirvish.el
+++ modules/dirvish.el
@@ -2,15 +2,14 @@
 
 (use-package dirvish
   :ensure t
-  :init
-  (dirvish-override-dired-mode)   ; make all Dired buffers Dirvish
   :custom
   ;; Enable icons, file info, and other visual enhancements
   (dirvish-attributes
-   '(nerd-icons          ; show icons (requires nerd-icons package)
-     file-size           ; show file sizes
+   '(
+     ;; nerd-icons          ; show icons (requires nerd-icons package)
+     ;; file-size           ; show file sizes
      collapse            ; smart path collapsing
-     git-msg             ; show git commit message
+     ;; git-msg             ; show git commit message
      file-time))         ; show modification time
 
   ;; Preview options
@@ -23,19 +22,26 @@
   (dirvish-side-width 35)
 
   :bind
-  (("M-1" . dirvish-side)
-   :map dirvish-mode-map
-   ("TAB" . dirvish-subtree-toggle)      ; tree structure: expand/collapse with TAB
-   ("M-TAB" . dirvish-subtree-up)        ; go up one level
-   ("M-n" . dirvish-narrow)              ; narrow to matching files
-   ("f" . dirvish-file-info-menu)        ; file info
-   ("y" . dirvish-yank-menu)             ; copy/move menu
-   ("s" . dirvish-quicksort)             ; sort options
-   ("?" . dirvish-dispatch))             ; action menu
+  ("M-1" . dirvish-side)
 
   :config
-  ;; Enable mouse support
-  (dirvish-peek-mode))
+  (require 'dirvish-extras)
+  (require 'dirvish-subtree)
+  (dirvish-override-dired-mode)
+  (dirvish-peek-mode)
+
+  ;; Evil keybindings - use evil-collection's method
+  (with-eval-after-load 'evil-collection-dired
+    (evil-collection-define-key 'normal 'dired-mode-map
+      "j" 'dired-next-line
+      "k" 'dired-previous-line
+      (kbd "TAB") 'dirvish-subtree-toggle
+      (kbd "M-TAB") 'dirvish-subtree-up
+      (kbd "M-n") 'dirvish-narrow
+      "f" 'dirvish-file-info-menu
+      "y" 'dirvish-yank-menu
+      "s" 'dirvish-quicksort
+      "?" 'dirvish-dispatch)))
 
 ;; Required for nerd-icons support
 (use-package nerd-icons
