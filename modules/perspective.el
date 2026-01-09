7e550dc8fc15c95360951c0931f3d8ffd5c16e47
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2026-01-09 23:07:33 +0100

    Store persp on exit

--- modules/perspective.el
+++ modules/perspective.el
@@ -12,13 +12,22 @@
   :custom
   (persp-initial-frame-name "scratch")
   (persp-mode-prefix-key (kbd "C-x x"))
+  (persp-state-default-file (expand-file-name "persp-state" user-emacs-directory))
   :config
-  (defun my/project-open-in-persp ()
-    "Open project in a perspective. Select ... to browse for a new project."
-    (interactive)
-    (let* ((input (completing-read "Project: " (cons "..." (project-known-project-roots)) nil t))
-           (dir (if (string= input "...") (read-directory-name "Directory: ") input))
-           (name (file-name-nondirectory (directory-file-name dir)))
-           (new-p (not (member name (persp-names)))))
-      (persp-switch name)
-      (when new-p (project-switch-project dir)))))
+  (add-hook 'kill-emacs-hook #'persp-state-save)
+  (add-hook 'emacs-startup-hook #'my/persp-state-restore))
+
+(defun my/persp-state-restore ()
+  "Restore perspective state from file if it exists."
+  (when (file-exists-p persp-state-default-file)
+    (persp-state-load persp-state-default-file)))
+
+(defun my/project-open-in-persp ()
+  "Open project in a perspective. Select ... to browse for a new project."
+  (interactive)
+  (let* ((input (completing-read "Project: " (cons "..." (project-known-project-roots)) nil t))
+         (dir (if (string= input "...") (read-directory-name "Directory: ") input))
+         (name (file-name-nondirectory (directory-file-name dir)))
+         (new-p (not (member name (persp-names)))))
+    (persp-switch name)
+    (when new-p (project-switch-project dir))))
