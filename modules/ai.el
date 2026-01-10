57105554545d6faac91c6ea08d508af9cbeefcfa
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2026-01-10 22:43:16 +0100

    Add gemini flash to gptel

--- modules/ai.el
+++ modules/ai.el
@@ -1,7 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package gptel
+  :hook (gptel-mode . gptel-highlight-mode)
+  :bind (("C-c RET" . gptel-send)
+         ("C-c r" . gptel-rewrite)
+         ("C-c ?" . gptel-menu))
   :config
+  ;; (setq gptel-model 'gpt-5.1)
+  (setq gptel-model 'gemini-3-flash-preview)
+
+  (gptel-make-xai "xAI"
+    :stream t
+    :key #'gptel-api-key-from-auth-source)
+
+  (gptel-make-gemini "Gemini"
+    :stream t
+    :key #'gptel-api-key-from-auth-source)
+
   (evil-define-key 'normal gptel-context-buffer-mode-map
     (kbd "q") #'gptel-context-quit
     (kbd "n") #'gptel-context-next
@@ -9,21 +24,9 @@
     (kbd "d") #'gptel-context-flag-deletion
     (kbd "RET") #'gptel-context-visit))
 
-;; Register xAI backend
-(gptel-make-xai "xAI"
-  :stream t
-  :key #'gptel-api-key-from-auth-source)
-
-;; Use OpenAI GPT-5.1 as default
-(setq gptel-model 'gpt-5.1)
-
-(global-set-key (kbd "C-c RET") 'gptel-send)
-(global-set-key (kbd "C-c r") 'gptel-rewrite)
-(global-set-key (kbd "C-c ?") 'gptel-menu)
-
 (use-package claude-code-ide
   :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
   :bind (("C-c C-<tab>" . claude-code-ide-menu)
          ("C-<tab>" . claude-code-ide-toggle))
   :config
-  (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP tools
+  (claude-code-ide-emacs-tools-setup))
