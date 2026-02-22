811b521494358b821d284f853bb132005e78aad4
Darius Kryszczuk <darius.kryszczuk@ataccama.com>
2026-02-22 12:33:06 +0100

    Add minibuffer content to separedit

--- modules/separedit.el
+++ modules/separedit.el
@@ -12,6 +12,26 @@
   (separedit-region-face ((t (:background nil))))
   :config
   (setq enable-recursive-minibuffers t)
-  (setq separedit-default-mode 'markdown-mode))
+  (setq separedit-default-mode 'markdown-mode)
+  (add-hook 'separedit-buffer-creation-hook #'my/separedit-inject-minibuffer-content))
+
+(defvar my/separedit--captured-content nil)
+
+(defun my/separedit-capture-context (&rest _)
+  (setq my/separedit--captured-content
+        (cond
+         ((minibufferp)
+          (minibuffer-contents))
+         ((eq major-mode 'vterm-mode)
+          (when-let* ((start (vterm--get-prompt-point)))
+            (string-trim (buffer-substring-no-properties start (point-max))))))))
+
+(advice-add 'separedit :before #'my/separedit-capture-context)
+
+(defun my/separedit-inject-minibuffer-content ()
+  (when (and my/separedit--captured-content
+             (= (buffer-size) 0))
+    (insert my/separedit--captured-content)
+    (setq my/separedit--captured-content nil)))
 
 (provide 'separedit)
